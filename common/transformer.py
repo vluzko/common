7e9698af2c4f94ca73b259848107cb65099d19c1
Vincent Luczkow <vincent.luczkow@gmail.com>
2022-01-25 00:48:19 -0500

    Updated

--- common/transformer.py
+++ common/transformer.py
@@ -1,21 +1,30 @@
+import torch
 from torch import nn
-
-
-class Transformer(nn.Module):
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def forward(self, inputs):
-        raise NotImplementedError
 
 
 class Encoder(nn.Module):
 
+    def __init__(self, layer_count: int) -> None:
+        super().__init__()
+        self.layer_count = layer_count
+        layers = (EncoderLayer() for _ in range(self.layer_count))
+
+    def forward(self, inputs: torch.Tensor):
+        raise NotImplementedError
+
+
+class EncoderLayer(nn.Module):
+
     def __init__(self) -> None:
         super().__init__()
+        self.attention = MultiHeadedAttention()
+        self.feed_forward = nn.Sequential(
+            nn.Linear(),
+            nn.ReLU(),
+            nn.Linear()
+        )
 
-    def forward(self, inputs):
+    def forward(self, inputs: torch.Tensor):
         raise NotImplementedError
 
 
@@ -24,13 +33,30 @@ class Decoder(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
+    def forward(self, inputs: torch.Tensor):
+        raise NotImplementedError
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, encoder: Encoder, decoder: Decoder) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, inputs: torch.Tensor):
+        raise NotImplementedError
+
+
+
+
 
 class MultiHeadedAttention(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, inputs):
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
 
