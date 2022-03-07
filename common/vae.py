c9fb728e4989c48e13c224192bae32c3d11c3584
vluzko <vincent.luczkow@gmail.com>
2022-03-07 17:03:11 -0500

    Updated

--- common/vae.py
+++ common/vae.py
@@ -11,7 +11,7 @@ from torchvision.utils import save_image
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=2, metavar='N',
                     help='number of epochs to train (default: 10)')
 # parser.add_argument('--no-cuda', action='store_true', default=False,
 #                     help='enables CUDA training')
