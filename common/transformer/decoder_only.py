04d14861cf3025e78b614d402d2e704eb578a2dc
vluzko <vincent.luczkow@gmail.com>
2022-04-05 23:08:57 -0400

    Updated

--- common/transformer/decoder_only.py
+++ common/transformer/decoder_only.py
@@ -1,109 +1,130 @@
-from itertools import product
+import math
 import torch
-from torch import nn
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+from einops import rearrange
+from torch import nn, optim
+from torch.nn import functional
+from torch.utils.data import DataLoader, TensorDataset
 
-
-class DTransformer(nn.Module):
-
-    def __init__(self, d_model: int, n_head: int, n_layers: int, n_tokens) -> None:
-        super().__init__()
-        self.embed = nn.Embedding(n_tokens, d_model)
-        self.pos_embed = PositionalEncoding()
-        self.decoder = Decoder(d_model, n_head, n_layers)
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        x = inputs
-        embedded = self.embed(x) + self.pos_embed(x)
-        decoded = self.decoder(x)
-        return decoded
+from typing import Optional
 
 
-class Decoder(nn.Module):
-    """The entire decoder"""
+class DecoderOnly(nn.Module):
+    """GPT"""
 
-    def __init__(self, d_model: int, n_head: int, n_layers: int) -> None:
+    def __init__(self, d_model: int, n_token: int, n_head: int, n_layers: int) -> None:
         super().__init__()
         self.d_model = d_model
+        self.n_token = n_token
         self.n_head = n_head
         self.n_layers = n_layers
+        self.token_embed = nn.Embedding(n_token, d_model)
+        self.pos_embed = PositionalEmbedding(d_model)
         self.layers = nn.Sequential(*[DecoderLayer(d_model, n_head) for _ in range(n_layers)])
+        self.linear = nn.Linear(d_model, n_token)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+    def forward(self, inputs: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        inputs = self.pos_embed(self.token_embed(inputs))
+        return self.linear(self.layers(inputs, mask))
 
 
 class DecoderLayer(nn.Module):
+    """A single decoder layer"""
+
     def __init__(self, d_model: int, n_head: int) -> None:
         super().__init__()
-        self.d_model = d_model
-        self.n_head = n_head
+        self.self_attn = MultiHeadAttention(d_model, n_head)
 
-        self.attention = MHAttention(d_model, n_head)
-
-        self.mlp = nn.Sequential(
-            nn.Linear(d_model, d_model),
-            nn.ReLU(),
-            nn.Linear(d_model, d_model)
-        )
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        # Attention
-        attn = self.attention(inputs)
-        output = self.mlp(inputs + attn)
-        return output
+    def forward(self, inputs: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        return inputs + self.self_attn(inputs, inputs, inputs)
 
 
-class MHAttention(nn.Module):
+class MultiHeadAttention(nn.Module):
+    """Multi headed attention"""
+
     def __init__(self, d_model: int, n_head: int) -> None:
         super().__init__()
-        self.q_proj = nn.Linear(d_model, d_model, bias=False)
-        self.k_proj = nn.Linear(d_model, d_model, bias=False)
-        self.v_proj = nn.Linear(d_model, d_model, bias=False)
-        self.scaled_dp = ScaledDotProductAttention(d_model, n_head)
-
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
-        q_p = self.q_proj(q)
-        k_p = self.k_proj(k)
-        v_p = self.v_proj(v)
-        scaled = self.scaled_dp(q_p, k_p, v_p)
-        return scaled
-
-
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self, d_model, n_head) -> None:
-        super().__init__()
+        self.q_w = nn.Linear(d_model, d_model)
+        self.k_w = nn.Linear(d_model, d_model)
+        self.v_w = nn.Linear(d_model, d_model)
+        self.sdp_attn = ScaledDotProductAttn(d_model)
         self.n_head = n_head
         assert d_model % n_head == 0
         self.d_head = d_model // n_head
 
-    def forward(self, q, k, v) -> torch.Tensor:
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            q: [seq_len, batch_size, d_model]
+        """
+        seq_len, batch_size, d_model = q.shape
+        def head_split(x):
+            return rearrange(x, 's b (nh dh) -> (b nh) s dh', nh=self.n_head, dh=self.d_head)
+        # Split by heads, rearrange so batch and n_head are on the outside
+        q_t = head_split(self.q_w(q))
+        k_t = head_split(self.k_w(k))
+        v_t = head_split(self.v_w(v))
 
-        raise NotImplementedError
+        # Apply scaled dot product attention and concatenate heads
+        return rearrange(self.sdp_attn(q_t, k_t, v_t, mask), '(b nh) s dh -> s b (nh dh)', nh=self.n_head, dh=self.d_head)
 
 
-class PositionalEncoding(nn.Module):
-    def __init__(self) -> None:
+
+class ScaledDotProductAttn(nn.Module):
+    """Scaled dot product attention"""
+
+    def __init__(self, d_model: int) -> None:
         super().__init__()
+        self.div = math.sqrt(d_model)
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        qk_t = torch.bmm(q, k.transpose(-2, -1)) / self.div
+
+        if mask is not None:
+            qk_t = qk_t.masked_fill(mask, -torch.inf)
+
+        return torch.bmm(functional.softmax(qk_t), v)
+
+
+class PositionalEmbedding(nn.Module):
+    """Sin/Cos positional embedding"""
+    pe: torch.Tensor
+
+    def __init__(self, d_model: int, max_len: int=27) -> None:
+        super().__init__()
+        numerator = torch.arange(max_len, dtype=torch.float32).view(-1, 1)
+        denominator = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) * (-math.log(10000) / d_model)).view(1, -1)
+        val = numerator @ denominator
+        pe = torch.empty(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(val)
+        pe[:, 0, 1::2] = torch.cos(val)
+        self.register_buffer('pe', pe)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
 
-def train(n_layers: int = 1, d_model: int=256, n_head: int=2):
-    dataset = generate_dataset()
-    n_tokens = dataset.shape[-1]
-    model = DTransformer(d_model, n_head, n_layers, n_tokens)
+        return inputs + self.pe[:inputs.shape[0]]
 
 
-def generate_dataset(upper: int=100) -> torch.Tensor:
-    left = torch.arange(upper).to(DEVICE).repeat(upper).view(upper * upper, 1)
-    right = torch.arange(upper).to(DEVICE).repeat_interleave(upper).view(upper * upper, 1)
-    sums = left + right
-    vals = torch.cat((left, right, left + right), dim=1)
-    import pdb
-    pdb.set_trace()
+def train(batch_size: int, d_model: int, n_token: int, n_head: int, n_layers: int=2) -> DecoderOnly:
+    # load data
+    data_loader = make_arithmetic_loader(batch_size)
+    model = DecoderOnly(d_model, n_token, n_head, n_layers)
+    opt = optim.Adam(model.parameters(), lr=0.001)
 
-generate_dataset()
+    for i, (seq, padding, output_len) in enumerate(data_loader):
+        # Construct mask
+        pred = model(seq[:-1], mask)
+        # TODO: Can't index like this. Needs to mask things so the padding just isn't used in the loss computation
+        loss = loss_fn(pred[-(output_len + padding): -padding], seq[-(output_len + padding): -padding])
+
+        loss.backward()
+        opt.step()
+
+    return model
 
 
+def make_arithmetic_loader(batch_size: int, num_samples: int) -> DataLoader:
+    for i in range(2, 6):
+        lhs = torch.randint(0, 1000, (num_samples, i))
+        rhs = torch.sum(lhs, dim=-1)
+    raise NotImplementedError
