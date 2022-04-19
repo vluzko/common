2201bcef4568db16b550972c2b4d388bc71b9894
vluzko <vincent.luczkow@gmail.com>
2022-04-18 22:06:38 -0400

    Updated

--- common/core/backprop.py
+++ common/core/backprop.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 
 class Module:
@@ -6,6 +7,8 @@ class Module:
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
+    def backward(self) -> torch.Tensor:
+        raise NotImplementedError
 
 
 class Linear(Module):
@@ -14,17 +17,32 @@ class Linear(Module):
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.weight = torch.zeros(self.input_dim, self.output_dim)
-        # TODO: initialize
+        self.weight = torch.zeros(self.input_dim, self.output_dim, requires_grad=False)
+        self.weight = nn.init.kaiming_uniform_(self.weight, mode='fan_in', nonlinearity='relu')
+        self.bias = torch.zeros(self.output_dim, requires_grad=False)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         output = torch.matmul(input, self.weight)
-        
-        raise NotImplementedError
+
+        return output + self.bias
+
+    def backward(self):
+        pass
 
 
 class ReLU(Module):
 
-    pass
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return torch.maximum(input, torch.zeros_like(input))
+
+    def backward(self):
+        raise NotImplementedError
 
 
+class MSELoss(Module):
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        return torch.mean((input - target) ** 2)
+
+    def backward(self):
+        raise NotImplementedError
