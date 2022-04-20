4e2e10a228da96e3522a391154f137f6cefae28b
vluzko <vincent.luczkow@gmail.com>
2022-04-19 22:02:23 -0400

    Updated

--- common/core/backprop.py
+++ common/core/backprop.py
@@ -7,7 +7,7 @@ class Module:
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
-    def backward(self) -> torch.Tensor:
+    def backward(self, *inputs: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
 
@@ -22,27 +22,78 @@ class Linear(Module):
         self.bias = torch.zeros(self.output_dim, requires_grad=False)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
+        self._last_activation = input
         output = torch.matmul(input, self.weight)
 
         return output + self.bias
 
-    def backward(self):
-        pass
+    def backward(self, next_err: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return self._last_activation * next_err
 
 
 class ReLU(Module):
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return torch.maximum(input, torch.zeros_like(input))
+        self._last_activation = input
+        return input * (input > 0).float()
 
-    def backward(self):
-        raise NotImplementedError
+    def backward(self, next_weight: torch.Tensor, next_err: torch.Tensor) -> torch.Tensor:  # type: ignore
+        """
+        Equation: W_{l+1}^T * error hadamard derivative(ReLU)(last_act)
+        """
+        # grad_act = torch.ones_like(self._last_activation) * (self._last_activation > 0).float()
+        act_deriv = self._last_activation > 0
+        grad_act = next_weight.transpose(0, 1) * next_err * act_deriv
+        return grad_act
 
 
 class MSELoss(Module):
 
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:  # type: ignore
+        self._last_activation = input
+        self._target = target
         return torch.mean((input - target) ** 2)
 
     def backward(self):
+        grad_act = 2 * (self._last_activation - self._target)
+        return grad_act
+
+
+class Network(Module):
+
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int):
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.layer_one = Linear(input_dim, hidden_dim)
+        self.act = ReLU()
+        self.layer_two = Linear(hidden_dim, output_dim)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output = self.layer_one.forward(input)
+        output = self.act.forward(output)
+        output = self.layer_two.forward(output)
+        return output
+
+    def backward(self, loss_back: torch.Tensor):
+        sec_layer = self.layer_two.backward(loss_back)
+        act_back = self.act.backward(self.layer_two.weight)
+
+    def update(self, weights):
         raise NotImplementedError
+
+
+def train():
+    net = Network(input_dim=2, hidden_dim=2, output_dim=1)
+    loss = MSELoss()
+
+    with torch.no_grad():
+        for i, (x, y) in dataset:
+            output = net.forward(x)
+            loss = loss.forward(x, y)
+            loss_back = loss.backward()
+            updates = net.backward(loss_back)
+
+
+if __name__ == '__main__':
+    train()
