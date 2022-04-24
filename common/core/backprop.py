ff3c29da252b4541d114fe60b4029d4762480673
vluzko <vincent.luczkow@gmail.com>
2022-04-24 00:49:46 -0400

    Updated

--- common/core/backprop.py
+++ common/core/backprop.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 import torch
-from torch import nn
+from torch import nn, optim
+from torch.nn import functional
 
 
 class Module:
@@ -18,20 +19,18 @@ class Linear(Module):
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.weight = torch.zeros(self.input_dim, self.output_dim, requires_grad=False)
-        self.weight = nn.init.kaiming_uniform_(self.weight, mode='fan_in', nonlinearity='relu')
+        self.weight = torch.arange(self.output_dim * self.input_dim, requires_grad=False).float().view(self.output_dim, self.input_dim)
+        # self.weight = nn.init.kaiming_uniform_(self.weight, mode='fan_in', nonlinearity='relu')
         self.bias = torch.zeros(self.output_dim, requires_grad=False)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         self._last_activation = input
-        output = torch.matmul(input, self.weight)
+        output = torch.matmul(input, self.weight.transpose(0, 1))
 
         return output + self.bias
 
     def cust_back(self, next_err: torch.Tensor) -> torch.Tensor:  # type: ignore
-        import pdb
-        pdb.set_trace()
-        return self._last_activation * next_err
+        return self._last_activation.view(-1, 1) * next_err
 
 
 class ReLU(Module):
@@ -46,7 +45,7 @@ class ReLU(Module):
         """
         # grad_act = torch.ones_like(self._last_activation) * (self._last_activation > 0).float()
         act_deriv = self._last_activation > 0
-        grad_act = next_weight.transpose(0, 1) * next_err * act_deriv
+        grad_act = next_weight.transpose(0, 1) @ next_err * act_deriv
         return grad_act
 
 
@@ -59,7 +58,7 @@ class MSELoss(Module):
 
     def cust_back(self):
         grad_act = 2 * (self._last_activation - self._target)
-        return grad_act
+        return 1 / grad_act.shape[0] * grad_act
 
 
 class Network(Module):
@@ -80,14 +79,39 @@ class Network(Module):
 
     def cust_back(self, loss_back: torch.Tensor):  # type: ignore
         sec_layer = self.layer_two.cust_back(loss_back)
+        import pdb
+        pdb.set_trace()
         act_back = self.act.cust_back(self.layer_two.weight, loss_back)
         first_layer = self.layer_one.cust_back(act_back)
         return first_layer, sec_layer
 
     def update(self, weights, lr: float=0.0001):
         w1, w2 = weights
-        self.layer_one.weight -= lr * w1
-        self.layer_two.weight -= lr * w2
+        self.layer_one.weight -= lr * w1.transpose(0, 1)
+        self.layer_two.weight -= lr * w2.transpose(0, 1)
+
+
+class TorchNet(nn.Module):
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.layer_one = nn.Linear(input_dim, hidden_dim)
+        self.layer_two = nn.Linear(hidden_dim, output_dim)
+
+    def copy_weights(self, other: Network):
+        with torch.no_grad():
+            self.layer_one.weight.copy_(other.layer_one.weight.detach().clone())
+            self.layer_one.bias.copy_(other.layer_one.bias.detach().clone())
+            self.layer_two.weight.copy_(other.layer_two.weight.detach().clone())
+            self.layer_two.bias.copy_(other.layer_two.bias.detach().clone())
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        output = self.layer_one(inputs)
+        output = torch.relu(output)
+        output = self.layer_two(output)
+        return output
 
 
 def make_data(size: int=1000) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -97,20 +121,62 @@ def make_data(size: int=1000) -> Tuple[torch.Tensor, torch.Tensor]:
     return ins, outs
 
 
-def train():
-    net = Network(input_dim=2, hidden_dim=2, output_dim=1)
-    loss = MSELoss()
-    ins, outs = make_data()
+def make_lin_data(size: int=1000, in_space: int=2) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    ins = torch.arange(size * in_space).float().view(size, -1)
+    weight = torch.randn(in_space, 1)
+    outs = torch.matmul(ins, weight)
+    return ins, outs, weight
 
-    with torch.no_grad():
+
+def train(size: int=10) -> None:
+    loss = MSELoss()
+    ins, outs = make_data(size)
+    net = Network(input_dim=ins.shape[1], hidden_dim=8, output_dim=outs.shape[1])
+    tnet = TorchNet(input_dim=ins.shape[1], hidden_dim=8, output_dim=outs.shape[1])
+    tnet.copy_weights(net)
+
+    # with torch.no_grad():
+    for i, (x, y) in enumerate(zip(ins, outs)):
+        output = net.forward(x)
+        toutput = tnet(x)
+        loss_val = loss.forward(output, y)
+        t_loss = functional.mse_loss(toutput, y)
+        t_loss.retain_grad()
+        assert torch.isclose(loss_val, t_loss)
+        loss_back = loss.cust_back()
+        updates = net.cust_back(loss_back)
+        t_loss.backward()
+        import pdb
+        pdb.set_trace()
+        net.update(updates)
+
+
+def lin_train(size: int=100, epochs: int = 1000, lr=1e-5) -> None:
+    loss = MSELoss()
+    ins, outs, weight = make_lin_data(size)
+    net = Linear(input_dim=ins.shape[1], output_dim=outs.shape[1])
+    # net = Network(input_dim=ins.shape[1], hidden_dim=8, output_dim=outs.shape[1])
+    # tnet = TorchNet(input_dim=ins.shape[1], hidden_dim=8, output_dim=outs.shape[1])
+    # tnet.copy_weights(net)
+
+    # with torch.no_grad():
+    for j in range(epochs):
+        indices = torch.randperm(ins.shape[0])
+        ins = ins[indices]
+        outs = outs[indices]
         for i, (x, y) in enumerate(zip(ins, outs)):
             output = net.forward(x)
-            loss_val = loss.forward(x, y)
+            # toutput = tnet(x)
+            loss_val = loss.forward(output, y)
+            print(loss_val)
             loss_back = loss.cust_back()
             updates = net.cust_back(loss_back)
-            net.update(updates)
-            print(i)
+            net.weight -= lr * updates.transpose(0, 1)
+        lr *= 0.8
+    import pdb
+    pdb.set_trace()
+
 
 
 if __name__ == '__main__':
-    train()
+    lin_train()
