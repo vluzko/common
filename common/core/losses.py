a3454df65e8671de2cde972e6b5727c9d9f96749
vluzko <vincent.luczkow@gmail.com>
2022-05-26 22:15:21 -0400

    Updated

---
+++ common/core/losses.py
@@ -0,0 +1,34 @@
+"""Common loss functions"""
+import torch
+
+
+def nll_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def kl_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def bce_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def bce_loss_logits(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def mse_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def poisson_nll(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def huber_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def hinge_loss(output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    raise NotImplementedError
