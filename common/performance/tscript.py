34df6e5238beeb7f1aae797852b75c05982bbe05
vluzko <vincent.luczkow@gmail.com>
2022-06-09 01:07:53 -0400

    Updated

---
+++ common/performance/tscript.py
@@ -0,0 +1,60 @@
+import torch
+
+
+class MyCell(torch.nn.Module):
+    def __init__(self):
+        super(MyCell, self).__init__()
+        self.linear = torch.nn.Linear(4, 4)
+
+    def forward(self, x, h):
+        new_h = torch.tanh(self.linear(x) + h)
+        return new_h, new_h
+
+my_cell = MyCell()
+x, h = torch.rand(3, 4), torch.rand(3, 4)
+traced_cell = torch.jit.trace(my_cell, (x, h))
+print(traced_cell)
+traced_cell(x, h)
+
+
+class MyDecisionGate(torch.nn.Module):
+    def forward(self, x):
+        if x.sum() > 0:
+            return x
+        else:
+            return -x
+
+
+class MyCell2(torch.nn.Module):
+    def __init__(self, dg):
+        super(MyCell2, self).__init__()
+        self.dg = dg
+        self.linear = torch.nn.Linear(4, 4)
+
+    def forward(self, x, h):
+        new_h = torch.tanh(self.dg(self.linear(x)) + h)
+        return new_h, new_h
+
+
+scripted_gate = torch.jit.script(MyDecisionGate())
+
+my_cell2 = MyCell2(scripted_gate)
+scripted_cell = torch.jit.script(my_cell2)
+
+print(scripted_gate.code)
+print(scripted_cell.code)
+
+
+class MyRNNLoop(torch.nn.Module):
+    def __init__(self):
+        super(MyRNNLoop, self).__init__()
+        self.cell = torch.jit.trace(MyCell(scripted_gate), (x, h))
+
+    def forward(self, xs):
+        h, y = torch.zeros(3, 4), torch.zeros(3, 4)
+        for i in range(xs.size(0)):
+            y, h = self.cell(xs[i], h)
+        return y, h
+
+rnn_loop = torch.jit.script(MyRNNLoop())
+print(rnn_loop.code)
